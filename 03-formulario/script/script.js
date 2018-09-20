fce9c20ff6f5e63977f32baabfb5ed1356eb69fd
Fernanda Vaz <fernandasvo@hotmail.com>
2018-09-19 22:33:20 -0300

    Add files via upload
    Desafio 3 - Javascript Formulário

---
+++ 03-formulario/script/script.js
@@ -0,0 +1,35 @@
+//Checagem do usuário----------------------------------------//
+
+var btnUser = document.querySelector("#btnuser");
+btnUser.addEventListener('click', function (){
+    var entradaUser= document.querySelector("#campouser").value;
+    alert("Insira seu nome");
+
+    // //*if (entradaUser.length==0){
+    //     alert("Insira seu nome");
+    // }
+    // else {
+    //     alert("Olá,"+ entrada)
+    // }*//
+})
+
+
+// //Checagem da senha------------------------------------------//
+
+// var btnSenha= document.querySelector(".btnsenha")
+// btnSenha.addEventListener('click', function (){
+//     var entradaSenha=document.querySelector("#camposenha").value;
+//     if (entradaSenha.length<6){
+//         alert("Sua senha deve conter pelo menos 6 caracteres")
+//     }
+//     else {
+//         alert("Tudo certo!")
+//     }
+// })
+
+// //Checagem de valor------------------------------------------//
+
+// var entradaValor= document.querySelector("#campovalor").value
+
+
+
